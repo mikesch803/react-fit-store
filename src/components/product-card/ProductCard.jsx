2a3f644d8e7d5c09f6fc37327fa7ddfbec323498
Mahendra Chauhan <mikesch803@gmail.com>
2022-03-21 22:07:51 +0530

    separate out product card

---
+++ src/components/product-card/ProductCard.jsx
@@ -0,0 +1,69 @@
+import React, { useContext } from "react";
+import { HeartIcon, RatingIcon, WishlistIcon } from "../../icons/icons";
+import { FilterContext, WishlistContext } from "../../context";
+import { CartContext } from "../../context/cart-context";
+import { Link } from "react-router-dom";
+export function ProductCard() {
+  const { getSortedArr } = useContext(FilterContext);
+  const { wishlistDataHandler, wishlistData } = useContext(WishlistContext);
+  const { dispatch, state } = useContext(CartContext);
+  return (
+    <div className="product-component">
+      <ul className="product-list">
+        {getSortedArr.map((item) => {
+          const { src, title, brand, offer_price, mrp, _id, rating } = item;
+          return (
+            <li key={_id}>
+              <div className="card card-vrt card-dismis">
+                <div className="card-img-container">
+                  <img className="card-img" src={src} alt={title} />
+                </div>
+                <div className="card-desc txt-left">
+                  <h3 className="card-desc-title">
+                    {title}
+                    <span className=" rating-num">
+                      {rating}
+                      <RatingIcon />
+                    </span>
+                  </h3>
+                  <p>by {brand}</p>
+                  <span className="card-price">Rs {offer_price}</span>
+                  <del className="card-mrp">Rs {mrp}</del>
+                  <div className="card-btns ">
+                    {state.cartData.includes(item) ? (
+                      <Link
+                        to="/cart"
+                        className="card-cart btn btn-primary txt-center"
+                      >
+                        go to cart
+                      </Link>
+                    ) : (
+                      <button
+                        className="card-cart btn btn-primary "
+                        onClick={() =>
+                          dispatch({ type: "ADD_TO_CART", payload: item })
+                        }
+                      >
+                        add to cart
+                      </button>
+                    )}
+                  </div>
+                </div>
+                <div
+                  className="card-dismis-btn"
+                  onClick={() => wishlistDataHandler(item)}
+                >
+                  {wishlistData.includes(item) ? (
+                    <HeartIcon />
+                  ) : (
+                    <WishlistIcon />
+                  )}
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
--- src/pages/product-page/Product.jsx
+++ src/pages/product-page/Product.jsx
@@ -1,60 +1,15 @@
-import React, { useContext } from "react";
 import { Aside, Header } from "../../components";
+import { ProductCard } from "../../components/product-card/ProductCard";
 import "./Product.css";
-import { HeartIcon, RatingIcon, WishlistIcon } from "../../icons/icons";
-import { FilterContext, WishlistContext } from "../../context";
 
 export function Product() {
-  const { getSortedArr } = useContext(FilterContext);
-  const { wishlistDataHandler, wishlistData } = useContext(WishlistContext);
   return (
     <div className="grid-layout-product">
       <Header />
       <Aside />
       <section className="product">
         <h2 className="product-section-title txt-left">Showing all products</h2>
-        <div className="product-component">
-          <ul className="product-list">
-            {getSortedArr.map((item) => {
-              const { src, title, brand, offer_price, mrp, _id, rating } = item;
-              return (
-                <li key={_id}>
-                  <div className="card card-vrt card-dismis">
-                    <div className="card-img-container">
-                      <img className="card-img" src={src} alt={title} />
-                    </div>
-                    <div className="card-desc txt-left">
-                      <h3 className="card-desc-title">
-                        {title}
-                        <span className=" rating-num">
-                          {rating}
-                          <RatingIcon />
-                        </span>
-                      </h3>
-                      <p>by {brand}</p>
-                      <span className="card-price">Rs {offer_price}</span>
-                      <del className="card-mrp">Rs {mrp}</del>
-                      <div className="card-btns ">
-                        <button className="card-cart btn btn-primary ">
-                          add to cart
-                        </button>
-                      </div>
-                    </div>
-                    <div className="card-dismis-btn"
-                      onClick={() => wishlistDataHandler(item)}
-                    >
-                      {wishlistData.includes(item) ? (
-                        <HeartIcon />
-                      ) : (
-                        <WishlistIcon />
-                      )}
-                    </div>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <ProductCard/>
       </section>
     </div>
   );
