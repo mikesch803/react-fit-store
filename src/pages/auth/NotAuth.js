ac3b429f52d3b7fa7a4df50b262700e3cde4b47f
Mahendra Chauhan <mikesch803@gmail.com>
2022-04-07 14:55:52 +0530

    Delete NotAuth.js

--- src/pages/auth/NotAuth.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-import { useLogin } from "../../context"
-
-
-export const NotAuth = () => {
-    const {login} = useLogin()
-    console.log(login)
-    return !login ? <Outlet/>:<Navigate to='/'/>
-}
