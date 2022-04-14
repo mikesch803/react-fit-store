b7febbcdc4f9a823ab3fb10c6119dd3eb59f15c1
Mahendra Chauhan <mikesch803@gmail.com>
2022-04-15 00:56:59 +0530

    Update useFilterModal.jsx

--- src/hooks/useFilterModal.jsx
+++ src/hooks/useFilterModal.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 export function useFilterModal() {
   const [asideStyles, setAsideStyles] = useState();
   const filterHandler = () => {
-    asideStyles === "block" ? setAsideStyles("none") : setAsideStyles("block");
+    setAsideStyles("block");
+    if(asideStyles === 'block') setAsideStyles('')
   };
   return { asideStyles, filterHandler };
 }
